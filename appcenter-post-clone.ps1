f864404635727fa667e81bf1133809d68213417f
tdevere <tdevere@microsoft.com>
2021-11-11 14:17:36 -0600

    overriding java vars

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,10 +1,15 @@
 Write-Host "#START Appcenter-post-clone.ps1"
 
-$env:JAVA_HOME
-$env:JAVA_HOME_11_X64
+Write-Host "JAVA_HOME IS " $env:JAVA_HOME
 
-$env:JAVA_HOME = $env:JAVA_HOME_11_X64
+Write-Host "AndroidSdkDirectory is " $env:AndroidSdkDirectory
+Write-Host "JavaSdkDirectory is " $env:JavaSdkDirectory
 
-Write-Host "JAVA_HOME NOW IS " $env:JAVA_HOME
+$env:AndroidSdkDirectory = $env:JAVA_HOME
+$env:JavaSdkDirectory == $env:JAVA_HOME
+
+Write-Host "AndroidSdkDirectory is " $env:AndroidSdkDirectory
+Write-Host "JavaSdkDirectory is " $env:JavaSdkDirectory
+
 
 Write-Host "#END Appcenter-post-clone.ps1"
