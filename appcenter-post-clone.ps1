7bb139de606a731680ba608aa5697bd90c5e15dc
tdevere <tdevere@microsoft.com>
2021-11-11 14:31:08 -0600

    _JavaSdkDirectory override

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -2,14 +2,14 @@ Write-Host "#START Appcenter-post-clone.ps1"
 
 Write-Host "JAVA_HOME IS " $env:JAVA_HOME
 
-Write-Host "AndroidSdkDirectory is " $env:AndroidSdkDirectory
-Write-Host "JavaSdkDirectory is " $env:JavaSdkDirectory
+#Write-Host "AndroidSdkDirectory is " $env:AndroidSdkDirectory
+#Write-Host "JavaSdkDirectory is " $env:JavaSdkDirectory
 
 $env:AndroidSdkDirectory = $env:JAVA_HOME
 $env:JavaSdkDirectory == $env:JAVA_HOME
 
-Write-Host "AndroidSdkDirectory is " $env:AndroidSdkDirectory
-Write-Host "JavaSdkDirectory is " $env:JavaSdkDirectory
+#Write-Host "AndroidSdkDirectory is " $env:AndroidSdkDirectory
+#Write-Host "JavaSdkDirectory is " $env:JavaSdkDirectory
 
 
 Write-Host "#END Appcenter-post-clone.ps1"
