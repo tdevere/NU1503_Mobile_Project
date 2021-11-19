abc029a5a943b753165ab6619d5fd336c6a6ebdb
tdevere <tdevere@microsoft.com>
2021-11-19 11:08:43 -0600

    i think this is it

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,4 +1,4 @@
-$bashPath = "$env:APPCENTER_SOURCE_DIRECTORY/appcenter-post-clone.sh"
+$bashPath = "$env:APPCENTER_SOURCE_DIRECTORY\appcenter-post-clone.sh"
 write-host $bashPath
 write-host $env:APPCENTER_SOURCE_DIRECTORY
 bash $bashPath
