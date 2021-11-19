d9f937c771df8669ae4a41eb36052897c1797645
tdevere <tdevere@microsoft.com>
2021-11-19 11:23:17 -0600

    added catch for shared project

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,5 +1,5 @@
 $SLN_PATH = Get-ChildItem -Path $env:APPCENTER_SOURCE_DIRECTORY -Include *.sln 
-$AllProjects = Get-ChildItem -Path $env:APPCENTER_SOURCE_DIRECTORY -Include *.csproj -Exclude *UWP*.csproj -Recurse
+$AllProjects = Get-ChildItem -Path $env:APPCENTER_SOURCE_DIRECTORY -Include *.csproj -Exclude "*UWP*.csproj", "NU1503_Mobile_Project.csproj" -Recurse
 write-host $SLN_PATH
 write-host $AllProjects
 foreach ($project in $AllProjects)
